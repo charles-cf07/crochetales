0f924402f66798c517c3fb4c2184c94c7e143ec4
charles-cf07 <charlesfinny2004@gmail.com>
2025-07-10 09:04:12 +0000

    added the flask project  & created app.py file

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', company_name='Crochetales')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug = True)
